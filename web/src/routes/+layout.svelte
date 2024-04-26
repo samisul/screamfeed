42f5693f484f1343ee1405e7c9db8f5db6ff9ee2
sami <sulsami@protonmail.com>
2024-04-27 00:40:50 +0200

    web: navigation bar

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,23 +1,26 @@
-<script>
+<script lang="ts">
   import '../app.postcss';
-  import { AppBar } from '@skeletonlabs/skeleton';
-
-  import { isLoggedIn } from '../stores/auth.store';
-  import { goto } from '$app/navigation';
+  import { TabAnchor, TabGroup } from '@skeletonlabs/skeleton';
+  import { NewspaperSolid, ProfileCardSolid } from 'flowbite-svelte-icons';
+  import { page } from '$app/stores';
 </script>
 
-<AppBar background="bg-transparent">
-  <svelte:fragment slot="lead"><span>screamfeed</span></svelte:fragment>
-  <svelte:fragment slot="trail">
-    <button
-      type="button"
-      class="btn btn-sm bg-transparent"
-      on:click={() => {
-        goto('/');
-      }}
-    >
-      <span>{$isLoggedIn ? 'Profile' : 'Sign-in'}</span>
-    </button>
-  </svelte:fragment>
-</AppBar>
-<slot />
+<TabGroup
+  justify="justify-center"
+  active="variant-filled-primary"
+  hover="hover:variant-soft-primary"
+  flex="flex-1 lg:flex-none"
+  rounded=""
+  border=""
+  class="bg-transparent w-full"
+>
+  <TabAnchor href="/" selected={$page.url.pathname === '/'}>
+    <svelte:fragment slot="lead"><ProfileCardSolid></ProfileCardSolid></svelte:fragment>
+  </TabAnchor>
+  <TabAnchor href="/feed" selected={$page.url.pathname === '/feed'}>
+    <svelte:fragment slot="lead"><NewspaperSolid></NewspaperSolid></svelte:fragment>
+  </TabAnchor>
+</TabGroup>
+<div class="m-auto flex max-w-4xl flex-1 flex-col">
+  <slot />
+</div>
---
+++ web/src/routes/feed/+page.svelte
@@ -0,0 +1,3 @@
+<script></script>
+
+<div>feed</div>
