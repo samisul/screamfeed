ea5ea391e6b618c072d4065a8770c52b8e7b7757
sami <sulsami@protonmail.com>
2024-04-27 15:24:42 +0200

    web: feed list

--- web/src/routes/feed/+page.svelte
+++ web/src/routes/feed/+page.svelte
@@ -14,9 +14,18 @@
 
 <div class="lg:p-4 p-2 flex flex-col justify-center align-middle items-center">
   {#await get() then data}
-    {#each data?.items ?? [] as item}
-      {item.url}
-    {/each}
+    <nav class="list-nav">
+      <ul>
+        {#each data?.items ?? [] as item}
+          <li>
+            <a href={'/feed/' + item.url}>
+              <!-- <span class="badge bg-primary-500">(icon)</span> -->
+              <span class="flex-auto">{item.title}</span>
+            </a>
+          </li>
+        {/each}
+      </ul>
+    </nav>
   {:catch error}
     <p style="color: red">{error.message}</p>
   {/await}
