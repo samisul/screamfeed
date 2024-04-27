ce571f335adf062017717ba61f9eb1f04ccd1b49
sami <sulsami@protonmail.com>
2024-04-27 10:42:18 +0200

    web: show a user's profile

--- web/src/routes/components/Profile.svelte
+++ web/src/routes/components/Profile.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { getUserOverview } from '$lib/user';
+  import { Avatar } from '@skeletonlabs/skeleton';
   import { isLoading } from '../../stores/global.store';
 
   async function getOverview() {
@@ -10,9 +11,11 @@
   }
 </script>
 
-<div class="lg:p-4">
+<div class="lg:p-4 p-2 flex flex-col justify-center align-middle items-center">
   {#await getOverview() then data}
-    {data?.name}
+    <Avatar src={data?.avatar ?? ''} width="w-32" rounded="rounded-none" />
+    <h1>{data?.name}</h1>
+    <p>{data?.email}</p>
   {:catch error}
     <p style="color: red">{error.message}</p>
   {/await}
