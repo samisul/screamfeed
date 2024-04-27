bd00b09c2e78e526e633e3d7f7f1cef74b533e15
sami <sulsami@protonmail.com>
2024-04-27 15:01:24 +0200

    web: initial feeds page

--- web/src/routes/feed/+page.svelte
+++ web/src/routes/feed/+page.svelte
@@ -1,3 +1,23 @@
-<script></script>
+<script lang="ts">
+  import type { FeedDto } from '$lib/feed/model';
+  import type { ListRes } from '$lib/global';
+  import { getFeedUrls } from '$lib/feed/index';
+  import { isLoading } from '../../stores/global.store';
 
-<div>feed</div>
+  async function get(): Promise<ListRes<FeedDto> | undefined> {
+    $isLoading = true;
+    const _urls = await getFeedUrls();
+    $isLoading = false;
+    return _urls;
+  }
+</script>
+
+<div class="lg:p-4 p-2 flex flex-col justify-center align-middle items-center">
+  {#await get() then data}
+    {#each data?.items ?? [] as item}
+      {item.url}
+    {/each}
+  {:catch error}
+    <p style="color: red">{error.message}</p>
+  {/await}
+</div>
