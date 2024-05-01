d7080b6414bbf25bba1c86635c0cbcf3176072ee
sami <sulsami@protonmail.com>
2024-05-01 22:55:18 +0200

    web: logout button

--- web/src/routes/components/Profile.svelte
+++ web/src/routes/components/Profile.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-  import { getUserOverview } from '$lib/user';
+  import { getUserOverview, logout } from '$lib/user';
   import { Avatar } from '@skeletonlabs/skeleton';
   import { isLoading } from '../../stores/global.store';
+  import {} from 'flowbite-svelte-icons';
 
   async function getOverview() {
     $isLoading = true;
@@ -11,7 +12,7 @@
   }
 </script>
 
-<div class="lg:p-4 p-2 flex flex-col justify-center align-middle items-center">
+<div class="lg:p-4 p-2 flex flex-col justify-center align-middle items-center gap-1">
   {#await getOverview() then data}
     <Avatar src={data?.avatar ?? ''} width="w-32" rounded="rounded-none" />
     <h1>{data?.name}</h1>
@@ -19,4 +20,5 @@
   {:catch error}
     <p style="color: red">{error.message}</p>
   {/await}
+  <button type="button" class="btn variant-filled-primary" on:click={logout}> Logout </button>
 </div>
